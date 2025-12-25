162e727186dcb5bd1e0ccab9f4d8ef02d9b75632
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-12-25 16:12:20 +0100

    feat: implement AddSpendingModule()

---
+++ apps/api/Modules/Spending/Spending/SpendingModule.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Spending;
+
+public static class SpendingModule
+{
+    public static IServiceCollection AddSpendingModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services
+        //    .AddApplicationServices()
+        //    .AddInfrastructureServices(configuration)
+        //    .AddApiServices(services);
+
+        return services;
+    }
+}
