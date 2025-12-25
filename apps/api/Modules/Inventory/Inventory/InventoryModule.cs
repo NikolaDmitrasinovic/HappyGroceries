f7555bc7cf23d40146d23dfeaaabf8470d2b0000
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-12-25 15:54:06 +0100

    feat: add invetory module service collection

---
+++ apps/api/Modules/Inventory/Inventory/InventoryModule.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Inventory;
+
+public static class InventoryModule
+{
+    public static IServiceCollection AddInventoryModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services
+        //    .AddApplicationServices()
+        //    .AddInfrastructureServices(configuration)
+        //    .AddApiServices(services);
+
+        return services;
+    }
+}
