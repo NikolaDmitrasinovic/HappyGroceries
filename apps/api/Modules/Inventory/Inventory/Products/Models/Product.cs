722cb8dc2dbfa25b3f80f582df1d2d6729b6f787
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-12-28 12:35:14 +0100

    feat: add product Create()

--- apps/api/Modules/Inventory/Inventory/Products/Models/Product.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Models/Product.cs
@@ -9,4 +9,22 @@ public class Product : Entity<Guid>
     public string Description { get; set; } = default!;
     public string ImageFile { get; set; } = default!;
     public decimal Price { get; set; }
+
+    public static Product Create(Guid id, string name,List<string> category,  decimal price, string description, string imageFile)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Category = category,
+            Description = description,
+            ImageFile = imageFile,
+            Price = price
+        };
+
+        return product;
+    }
 }
