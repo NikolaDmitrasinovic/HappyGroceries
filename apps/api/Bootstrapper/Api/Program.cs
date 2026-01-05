0703534bce7dfa1474ea4806af0ca5344542d7e5
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-05 18:08:44 +0100

    feat: add health check endpiont

--- apps/api/Bootstrapper/Api/Program.cs
+++ apps/api/Bootstrapper/Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Net.Sockets;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,13 @@ builder.Services
     .AddSpendingModule(builder.Configuration);
 
 var app = builder.Build();
+
+var host = builder.Configuration["Db:Host"];
+var port = int.Parse(builder.Configuration["Db:Port"]!);
+
+using var tcpClient = new TcpClient(host, port);
+
+app.MapGet("/health", () => Results.Ok("API is up and DB port is reachable"));
 
 // Configure the HTTP request pipeline.
 app
