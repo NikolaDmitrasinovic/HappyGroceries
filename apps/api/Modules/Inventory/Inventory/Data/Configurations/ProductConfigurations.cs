5354982c4f16fe2891876455f063dc2a848d40ab
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-08 21:19:42 +0100

    feat: add product configuration

---
+++ apps/api/Modules/Inventory/Inventory/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inventory.Data.Configurations;
+
+public class ProductConfigurations : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
+        builder.Property(p => p.Category).IsRequired();
+        builder.Property(p => p.Description).HasMaxLength(250);
+        builder.Property(p => p.ImageFile).HasMaxLength(200);
+        builder.Property(p => p.Price);
+        builder.Property(p => p.Stock).IsRequired();
+        builder.Property(p => p.Threshold).IsRequired();
+    }
+}
