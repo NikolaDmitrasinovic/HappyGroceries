eead36e21970459888d86d4e8d0dc11a836a73fc
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-09 22:51:29 +0100

    feat: add seed data

---
+++ apps/api/Modules/Inventory/Inventory/Data/Seed/InitialData.cs
@@ -0,0 +1,10 @@
+﻿namespace Inventory.Data.Seed;
+
+public static class InitialData
+{
+    public static IEnumerable<Product> Products =>
+        [
+            Product.Create(new Guid("382c74c3-721d-4f34-80e5-57657b6cbc27"), "Pasta", [], 125, "", "", 5, 2),
+            Product.Create(new Guid("382c74c3-721d-4f34-80e5-57657b6cbc28"), "Sausage", [], 350, "", "", 3, 1)
+        ];
+}
