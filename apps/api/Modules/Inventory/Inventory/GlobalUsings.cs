ffc7c7e2fa39d5d4c94fd71b80b7e4d9d829dff6
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-09 20:03:08 +0100

    refactor: extract global using

--- apps/api/Modules/Inventory/Inventory/GlobalUsings.cs
+++ apps/api/Modules/Inventory/Inventory/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using Inventory.Products.Models;
 global using Microsoft.EntityFrameworkCore;
 global using System.Reflection;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Inventory.Data;
