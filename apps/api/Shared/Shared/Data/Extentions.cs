6e6823ed1b0883324242e453c131840f2b5b22dd
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-10 17:01:26 +0100

    feat: add generic seed data method

--- apps/api/Shared/Shared/Data/Extentions.cs
+++ apps/api/Shared/Shared/Data/Extentions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Seed;
 
 namespace Shared.Data;
 
@@ -11,16 +12,28 @@ public static class Extentions
     {
         MigrateDatabaseAsync<TContext>(app.ApplicationServices).GetAwaiter().GetResult();
 
+        SeedDataAsync(app.ApplicationServices).GetAwaiter().GetResult();
+
         return app;
     }
 
-    private static async Task MigrateDatabaseAsync<TContext>(IServiceProvider serviceProvider) 
+    private static async Task MigrateDatabaseAsync<TContext>(IServiceProvider serviceProvider)
         where TContext : DbContext
     {
         using var scope = serviceProvider.CreateScope();
 
         var context = scope.ServiceProvider.GetRequiredService<TContext>();
-
         await context.Database.MigrateAsync();
     }
+
+    private static async Task SeedDataAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+
+        var seeders = scope.ServiceProvider.GetServices<IDataSeeder>();
+        foreach (var seeder in seeders)
+        {
+            await seeder.SeedAllAsync();
+        }
+    }    
 }
