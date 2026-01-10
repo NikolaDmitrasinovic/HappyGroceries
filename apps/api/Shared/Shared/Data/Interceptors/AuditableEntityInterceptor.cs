935fc235ff78112e84b71803a82302512ec929ad
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-10 20:28:43 +0100

    feat: add audtable entity interceptor feat: override SavingChanges feat: everride SavingChangesAsync

---
+++ apps/api/Shared/Shared/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Shared.DDD;
+
+namespace Shared.Data.Interceptors;
+
+public class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        UpdateEntitites(eventData.Context);
+        return base.SavingChanges(eventData, result);
+    }    
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        UpdateEntitites(eventData.Context);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private static void UpdateEntitites(DbContext? context)
+    {
+        if (context == null) return;
+
+        foreach (var entry in context.ChangeTracker.Entries<IEntity>())
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedBy = "system";
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+            }
+
+            if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
+            {
+                entry.Entity.LastModifiedBy = "system";
+                entry.Entity.LastModified = DateTime.UtcNow;
+            }
+        }
+    }
+}
