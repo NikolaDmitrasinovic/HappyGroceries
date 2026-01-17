b4059853808a9941aa331df2454cbf20280a1592
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-17 20:08:41 +0100

    feat: add mediator

---
+++ apps/api/Shared/Shared/Messaging/Mediator.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared.Messaging;
+
+public interface IMediator
+{
+    Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+    Task Publish(INotification notification, CancellationToken cancellationToken = default);
+}
+
+public class Mediator : IMediator
+{
+    public Task Publish(INotification notification, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
