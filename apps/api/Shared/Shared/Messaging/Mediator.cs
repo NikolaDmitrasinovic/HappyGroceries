3509ba733d7a84faa5c821640f79fbfd02d97ca5
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-17 20:16:29 +0100

    feat: implement send

--- apps/api/Shared/Shared/Messaging/Mediator.cs
+++ apps/api/Shared/Shared/Messaging/Mediator.cs
@@ -1,4 +1,7 @@
-﻿namespace Shared.Messaging;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared.Messaging;
 
 public interface IMediator
 {
@@ -6,15 +9,23 @@ public interface IMediator
     Task Publish(INotification notification, CancellationToken cancellationToken = default);
 }
 
-public class Mediator : IMediator
+public class Mediator(IServiceProvider serviceProvider) : IMediator
 {
+    public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var handlerType = typeof(IRequestHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
+        var handler = serviceProvider.GetRequiredService(handlerType);
+
+        var method = handlerType.GetMethod(nameof(IRequestHandler<,>.Handle))!;
+        var taskObj = method.Invoke(handler, [request, cancellationToken])!;
+
+        return (Task<TResponse>)taskObj;
+    }
+
     public Task Publish(INotification notification, CancellationToken cancellationToken = default)
     {
         throw new NotImplementedException();
-    }
-
-    public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
+    }    
 }
