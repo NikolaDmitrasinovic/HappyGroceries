7b286bfbae45cb7d608e2dee715d6461c516dac2
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-18 20:52:45 +0100

    feat: wrap messaging handler into command handler

---
+++ apps/api/Shared/Shared/CQRS/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Messaging;
+
+namespace Shared.CQRS;
+
+public interface ICommandHandler<in TCommand>
+    : ICommandHanler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+}
+
+public interface ICommandHanler<in TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
