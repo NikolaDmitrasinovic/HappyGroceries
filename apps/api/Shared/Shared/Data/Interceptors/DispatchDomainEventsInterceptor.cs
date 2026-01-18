c40ab02d9c0dc1fcce03808a3a4a5dabb8b28872
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-18 20:43:24 +0100

    refactor: replace MediatR with messaging

--- apps/api/Shared/Shared/Data/Interceptors/DispatchDomainEventsInterceptor.cs
+++ apps/api/Shared/Shared/Data/Interceptors/DispatchDomainEventsInterceptor.cs
@@ -1,7 +1,7 @@
-﻿using MediatR;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Shared.DDD;
+using Shared.Messaging;
 
 namespace Shared.Data.Interceptors;
 
