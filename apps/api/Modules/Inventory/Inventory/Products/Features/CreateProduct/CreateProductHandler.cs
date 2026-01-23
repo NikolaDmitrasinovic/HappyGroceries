41806c07d54b1be03f80b49c885749f35fe44054
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-23 20:37:42 +0100

    feat: implement handler

--- apps/api/Modules/Inventory/Inventory/Products/Features/CreateProduct/CreateProductHandler.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -1,10 +1,30 @@
-﻿namespace Inventory.Products.Features.CreateProduct;
+﻿using Inventory.Products.Dtos;
 
-public record CreateProductCommand
+namespace Inventory.Products.Features.CreateProduct;
+
+public record CreateProductCommand(ProductDto Product)
     : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductHandler
+internal class CreateProductHandler(InventoryDbContext dbContext)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = Product.Create(
+            Guid.NewGuid(),
+            command.Product.Name,
+            command.Product.Category,
+            command.Product.Price,
+            command.Product.Description,
+            command.Product.ImageFile,
+            command.Product.Stock,
+            command.Product.Threshold);
+
+        dbContext.Products.Add(product);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
+    }
 }
