877928e0443a08843571772b59684e8a7dae1fca
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-23 20:26:29 +0100

    feat: add command

--- apps/api/Modules/Inventory/Inventory/Products/Features/CreateProduct/CreateProductHandler.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,10 @@
 ﻿namespace Inventory.Products.Features.CreateProduct;
 
+public record CreateProductCommand
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
 internal class CreateProductHandler
 {
 }
