c6db7dd58e948b1e4b0be3f615c0cc0727e494ad
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-24 18:26:40 +0100

    feat: add product command

--- apps/api/Modules/Inventory/Inventory/Products/Features/UpdateProduct/UpdateProductHandler.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/UpdateProduct/UpdateProductHandler.cs
@@ -1,5 +1,10 @@
 ﻿namespace Inventory.Products.Features.UpdateProduct;
 
+public record UpdateProductCommand(ProductDto ProductDto)
+    : ICommand<UpdateProductResult>;
+
+public record UpdateProductResult(ProductDto ProductDto);
+
 internal class UpdateProductHandler
 {
 }
