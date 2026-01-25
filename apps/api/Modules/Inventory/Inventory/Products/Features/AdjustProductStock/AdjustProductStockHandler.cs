8795a4ad49368d75678db12fb8f1db79345117ef
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-25 10:16:57 +0100

    feat: add command

--- apps/api/Modules/Inventory/Inventory/Products/Features/AdjustProductStock/AdjustProductStockHandler.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/AdjustProductStock/AdjustProductStockHandler.cs
@@ -1,5 +1,10 @@
 ﻿namespace Inventory.Products.Features.AdjustProductStock;
 
+public record AdjustProductStockCommand(Guid Id, decimal Delta)
+    : ICommand<AdjustProductStockResult>;
+
+public record AdjustProductStockResult(Guid Id);
+
 internal class AdjustProductStockHandler
 {
 }
