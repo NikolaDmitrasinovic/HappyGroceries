98236ab3e7b37a79054b7869505d8adcd7c6ff66
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-26 21:13:11 +0100

    refactor: cleanup product configurations

--- apps/api/Modules/Inventory/Inventory/Data/Configurations/ProductConfigurations.cs
+++ apps/api/Modules/Inventory/Inventory/Data/Configurations/ProductConfigurations.cs
@@ -6,10 +6,6 @@ public class ProductConfigurations : IEntityTypeConfiguration<Product>
     {
         builder.HasKey(p => p.Id);
         builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
-        builder.Property(p => p.Category).IsRequired();
-        builder.Property(p => p.Description).HasMaxLength(250);
-        builder.Property(p => p.ImageFile).HasMaxLength(200);
-        builder.Property(p => p.Price);
         builder.Property(p => p.Stock).IsRequired();
         builder.Property(p => p.Threshold).IsRequired();
     }
