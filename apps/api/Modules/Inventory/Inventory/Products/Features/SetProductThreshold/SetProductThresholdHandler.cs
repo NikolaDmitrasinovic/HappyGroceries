1ce7059fb39ed6b97e2baf6a2412e62f9b28450c
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-01-26 18:38:44 +0100

    feat: add command

--- apps/api/Modules/Inventory/Inventory/Products/Features/SetProductThreshold/SetProductThresholdHandler.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/SetProductThreshold/SetProductThresholdHandler.cs
@@ -1,5 +1,10 @@
 ﻿namespace Inventory.Products.Features.SetProductThreshold;
 
+public record SetProductThresholdCommand(Guid Id, decimal Threshold)
+    : ICommand<SetProductThresholdResult>;
+
+public record SetProductThresholdResult(Guid Id);
+
 internal class SetProductThresholdHandler
 {
 }
