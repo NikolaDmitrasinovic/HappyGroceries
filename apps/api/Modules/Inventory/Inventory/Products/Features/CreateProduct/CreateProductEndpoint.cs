f15ff38b9002eaeffb3acf1442d7e05340408632
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-01 20:13:43 +0100

    feat: add request feat: add response

---
+++ apps/api/Modules/Inventory/Inventory/Products/Features/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Routing;
+
+namespace Inventory.Products.Features.CreateProduct;
+
+public record CreateProductRequest(ProductDto Product);
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
+}
