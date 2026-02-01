1eaa3af90bf5938a4a4f3e19585f25349bab05fb
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-01 20:24:10 +0100

    feat: implement route

--- apps/api/Modules/Inventory/Inventory/Products/Features/CreateProduct/CreateProductEndpoint.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,8 @@
 ﻿using Carter;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Shared.Messaging;
 
 namespace Inventory.Products.Features.CreateProduct;
 
@@ -10,6 +13,20 @@ public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        throw new NotImplementedException();
+        app.MapPost("/products", async (CreateProductRequest requset, IMediator sender) =>
+        {
+            var command = new CreateProductCommand(requset.Product);
+
+            var result = await sender.Send(command);
+
+            var response = new CreateProductResponse(result.Id);
+
+            return Results.Created();
+        })
+        .WithName("CreteProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
     }
 }
