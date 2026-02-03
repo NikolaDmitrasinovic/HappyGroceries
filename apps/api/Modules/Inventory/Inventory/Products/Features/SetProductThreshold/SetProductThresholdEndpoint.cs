16d92adfb47516e1872d455574bf5ae52f3670cd
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-03 11:07:31 +0100

    feat: add request feat: add response

---
+++ apps/api/Modules/Inventory/Inventory/Products/Features/SetProductThreshold/SetProductThresholdEndpoint.cs
@@ -0,0 +1,8 @@
+﻿namespace Inventory.Products.Features.SetProductThreshold;
+
+public record SetProductThresholdRequest();
+public record SetProductThresholdResponse();
+
+public class SetProductThresholdEndpoint
+{
+}
