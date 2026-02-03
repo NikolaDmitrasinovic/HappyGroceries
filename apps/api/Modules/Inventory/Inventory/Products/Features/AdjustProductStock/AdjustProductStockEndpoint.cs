1695d06259ca0aa6ff6f1acfbe654caa35a7b9fb
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-03 10:41:08 +0100

    feat: implement route

--- apps/api/Modules/Inventory/Inventory/Products/Features/AdjustProductStock/AdjustProductStockEndpoint.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/AdjustProductStock/AdjustProductStockEndpoint.cs
@@ -1,8 +1,33 @@
-﻿namespace Inventory.Products.Features.AdjustProductStock;
+﻿using Carter;
+using Inventory.Products.Features.CreateProduct;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Shared.Messaging;
 
-public record AdjustProductStockRequest();
-public record AdjustProductStockResponse();
+namespace Inventory.Products.Features.AdjustProductStock;
 
-public class AdjustProductStockEndpoint
+public record AdjustProductStockRequest(Guid Id, int Delta);
+public record AdjustProductStockResponse(Guid Id);
+
+public class AdjustProductStockEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPatch("/products/stock", async (AdjustProductStockRequest requset, IMediator sender) =>
+        {
+            var command = new AdjustProductStockCommand(requset.Id, requset.Delta);
+
+            var result = await sender.Send(command);
+
+            var response = new AdjustProductStockResponse(result.Id);
+
+            return Results.Created<AdjustProductStockResponse>($"/products/{response.Id}", response);
+        })
+        .WithName("AdjustProductStock")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Adjust Product Stock")
+        .WithDescription("Adjust Product Stock");
+    }
 }
