e5f1eff1bcaace315d9192306794818a25ad3e2c
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-03 10:34:43 +0100

    feat: add request feat: add response

---
+++ apps/api/Modules/Inventory/Inventory/Products/Features/AdjustProductStock/AdjustProductStockEndpoint.cs
@@ -0,0 +1,8 @@
+﻿namespace Inventory.Products.Features.AdjustProductStock;
+
+public record AdjustProductStockRequest();
+public record AdjustProductStockResponse();
+
+public class AdjustProductStockEndpoint
+{
+}
