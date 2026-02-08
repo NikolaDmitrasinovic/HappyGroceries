50789bd10d6bfa2bdbc527c6e4fb403c8188f10d
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-08 16:19:07 +0100

    feat: add query

---
+++ apps/api/Modules/Inventory/Inventory/Products/Features/GetLowStockProducts/GetLowStockProductsHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Inventory.Products.Features.GetLowStockProducts;
+
+public record GetLowStockProductQuery() : IQuery<GetLowStockProductsResult>;
+
+public record GetLowStockProductsResult(IEnumerable<ProductDto> Products);
+
+internal class GetLowStockProductsHandler
+{
+}
