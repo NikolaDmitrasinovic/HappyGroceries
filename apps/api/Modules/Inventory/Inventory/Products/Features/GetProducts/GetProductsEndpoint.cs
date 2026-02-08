9123ff14c37cd7428711c959e37c0f714a7e0d70
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-08 09:02:42 +0100

    feat: add endpoint

--- apps/api/Modules/Inventory/Inventory/Products/Features/GetProducts/GetProductsEndpoint.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/GetProducts/GetProductsEndpoint.cs
@@ -1,8 +1,34 @@
-﻿namespace Inventory.Products.Features.GetProducts;
+﻿using Carter;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Shared.Messaging;
+
+namespace Inventory.Products.Features.GetProducts;
 
 public record GetProductsResponse(IEnumerable<ProductDto> Products);
 
-
-internal class GetProductsEndpoint
+public class GetProductsEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (IMediator sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+
+            var response = new List<ProductDto>();
+            foreach(var product in result.Products)
+            {
+                var productDto = new ProductDto(product.Name, product.Stock, product.Threshold);
+                response.Add(productDto);
+            }
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
+    }
 }
