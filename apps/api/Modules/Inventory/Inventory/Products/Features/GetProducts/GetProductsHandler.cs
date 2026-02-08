c4e897527edd31606cc97afe5a65148b28e570a3
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-08 08:44:04 +0100

    feat: add query

---
+++ apps/api/Modules/Inventory/Inventory/Products/Features/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Inventory.Products.Features.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+
+public record GetProductsResult(IEnumerable<ProductDto> Products);
+
+internal class GetProductsHandler
+{
+}
