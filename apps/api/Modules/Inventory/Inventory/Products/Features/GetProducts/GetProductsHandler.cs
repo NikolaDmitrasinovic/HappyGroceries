bc86eb182fa1ed9cd8bd99bb941b6c8c19d393ba
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-08 08:50:50 +0100

    feat: implement handler

--- apps/api/Modules/Inventory/Inventory/Products/Features/GetProducts/GetProductsHandler.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/GetProducts/GetProductsHandler.cs
@@ -1,9 +1,27 @@
-﻿namespace Inventory.Products.Features.GetProducts;
+﻿
+namespace Inventory.Products.Features.GetProducts;
 
 public record GetProductsQuery() : IQuery<GetProductsResult>;
 
 public record GetProductsResult(IEnumerable<ProductDto> Products);
 
-internal class GetProductsHandler
+internal class GetProductsHandler(InventoryDbContext dbContext)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        var products = await dbContext.Products
+            .AsNoTracking()
+            .OrderBy(p => p.Name)
+            .ToListAsync(cancellationToken);
+
+        var productDtos = new List<ProductDto>();
+        foreach (var product in products)
+        {
+            var productDto = new ProductDto(product.Name, product.Stock, product.Threshold);
+            productDtos.Add(productDto);
+        }
+
+        return new GetProductsResult(productDtos);
+    }
 }
