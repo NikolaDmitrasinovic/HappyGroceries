636f108f94a9b33809f9d8e94f1268fa3da0e9fe
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-08 16:21:36 +0100

    feat: implement handler

--- apps/api/Modules/Inventory/Inventory/Products/Features/GetLowStockProducts/GetLowStockProductsHandler.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/GetLowStockProducts/GetLowStockProductsHandler.cs
@@ -1,9 +1,27 @@
 ﻿namespace Inventory.Products.Features.GetLowStockProducts;
 
-public record GetLowStockProductQuery() : IQuery<GetLowStockProductsResult>;
+public record GetLowStockProductsQuery() : IQuery<GetLowStockProductsResult>;
 
 public record GetLowStockProductsResult(IEnumerable<ProductDto> Products);
 
-internal class GetLowStockProductsHandler
+internal class GetLowStockProductsHandler(InventoryDbContext dbContext)
+    : IQueryHandler<GetLowStockProductsQuery, GetLowStockProductsResult>
 {
+    public async Task<GetLowStockProductsResult> Handle(GetLowStockProductsQuery request, CancellationToken cancellationToken)
+    {
+        var products = await dbContext.Products
+            .AsNoTracking()
+            .Where(p => p.IsLowStock)
+            .OrderBy(p => p.Name)
+            .ToListAsync(cancellationToken);
+
+        var productDtos = new List<ProductDto>();
+        foreach (var product in products)
+        {
+            var productDto = new ProductDto(product.Name, product.Stock, product.Threshold);
+            productDtos.Add(productDto);
+        }
+
+        return new GetLowStockProductsResult(productDtos);
+    }
 }
