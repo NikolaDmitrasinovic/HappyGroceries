718a09ad05c940fe3e016306677c81d6692365e5
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-11 12:26:43 +0100

    feat: add get products route

--- apps/api/Modules/Inventory/Inventory/Products/Controllers/ProductsController.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Controllers/ProductsController.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Inventory.Products.Features.GetProducts;
+using Microsoft.AspNetCore.Mvc;
 using Shared.Messaging;
 
 namespace Inventory.Products.Controllers;
 
+[ApiController]
+[Route("products")]
 public class ProductsController(IMediator mediator) : ControllerBase
 {
+    [HttpGet]
+    public async Task<ActionResult<GetProductsResponse>> GetProducts(CancellationToken cancellationToken)
+    {
+        var resonse = mediator.Send(new GetProductsQuery(), cancellationToken);
+        return Ok(resonse);
+    }
 }
