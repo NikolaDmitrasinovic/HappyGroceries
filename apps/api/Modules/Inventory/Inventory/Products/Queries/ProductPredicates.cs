00c84b70dd424d7a67297c6b9bfac6c8c3294bc8
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-22 18:37:35 +0100

    feat: add expression

---
+++ apps/api/Modules/Inventory/Inventory/Products/Queries/ProductPredicates.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace Inventory.Products.Queries;
+
+public class ProductPredicates
+{
+    public static readonly Expression<Func<Product, bool>> IsLowStockExpression =
+        p => p.Stock <= p.Threshold;
+}
