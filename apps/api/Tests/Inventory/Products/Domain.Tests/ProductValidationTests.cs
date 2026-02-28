65655e1793682d272a17bbc2cbc2c88e0b3cd36f
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-02-28 01:34:03 +0100

    test: set threhold throws when negative

--- apps/api/Tests/Inventory/Products/Domain.Tests/ProductValidationTests.cs
+++ apps/api/Tests/Inventory/Products/Domain.Tests/ProductValidationTests.cs
@@ -14,4 +14,17 @@ public class ProductValidationTests
         // Assert
         Assert.Equal("stock ('-5') must be a non-negative value. (Parameter 'stock')\r\nActual value was -5.", result.Message);
     }
+
+    [Fact]
+    public static void SetThreshold_throws_when_negative()
+    {
+        // Arrange
+        var product = ProductTestFactory.CreateProductHelper();
+
+        // Act
+        var result = Assert.Throws<ArgumentOutOfRangeException>(() => product.SetThreshold(-5));
+
+        // Assert
+        Assert.Equal("threshold ('-5') must be a non-negative value. (Parameter 'threshold')\r\nActual value was -5.", result.Message);
+    }
 }
