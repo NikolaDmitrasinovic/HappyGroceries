34587d23053fae77bc8fa20fc4f60d17c40815f7
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-01 15:30:58 +0100

    test: create throws when name null or empty

---
+++ apps/api/Tests/Inventory/Products/Domain.Tests/ProductCreationTests.cs
@@ -0,0 +1,18 @@
+﻿using Inventory.Products.Models;
+
+namespace Domain.Tests;
+
+public class ProductCreationTests
+{
+    [Fact]
+    public void Create_Throws_When_Name_Null_or_Empty()
+    {
+        // Arrange
+
+        // Act
+        var exception = Assert.Throws<ArgumentException>(() =>Product.Create(""));
+
+        // Assert
+        Assert.Equal("name", exception.ParamName);
+    }
+}
