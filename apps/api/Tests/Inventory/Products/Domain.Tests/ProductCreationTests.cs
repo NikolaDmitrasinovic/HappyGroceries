e8ef15fb14d20e63bb7576155a0efea23ec40063
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-01 19:33:00 +0100

    chore: rename test test: create raises event

--- apps/api/Tests/Inventory/Products/Domain.Tests/ProductCreationTests.cs
+++ apps/api/Tests/Inventory/Products/Domain.Tests/ProductCreationTests.cs
@@ -1,11 +1,12 @@
-﻿using Inventory.Products.Models;
+﻿using Inventory.Products.Events;
+using Inventory.Products.Models;
 
 namespace Domain.Tests;
 
 public class ProductCreationTests
 {
     [Fact]
-    public void Create_Throws_When_Name_Null_or_Empty()
+    public void Create_Throws_When_Name_Empty()
     {
         // Arrange
 
@@ -44,5 +45,18 @@ public class ProductCreationTests
         // Assert
         Assert.IsType<Guid>(product.Id);
         Assert.NotEqual(Guid.Empty, product.Id);
+    }
+
+    [Fact]
+    public void Create_Raises_ProductCreatedEvent()
+    {
+        // Arrange
+        var product = Product.Create("NewProduct");
+
+        // Act
+        var events = product.ClearDomainEvents();
+
+        // Assert
+        Assert.IsType<ProductCreatedEvent>(events[0]);
     }
 }
