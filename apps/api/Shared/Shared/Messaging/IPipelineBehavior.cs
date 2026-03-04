86baa26ea3992391061b7479734e60ea54635c57
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-04 19:52:51 +0100

    feat: add IPipelineBehavior feat: add RequestHandlerDelegate

--- apps/api/Shared/Shared/Messaging/IPipelineBehavior.cs
+++ apps/api/Shared/Shared/Messaging/IPipelineBehavior.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Shared.Messaging;
 
-namespace Shared.Messaging;
-
-internal interface IPipelineBehavior
+public interface IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
 {
+    Task<TResponse> HandleAsync(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken);
 }
+
+public delegate Task<TResponse> RequestHandlerDelegate<TResponse>();
