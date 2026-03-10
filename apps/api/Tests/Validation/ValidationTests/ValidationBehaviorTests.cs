0a127f86cf8622695eea8c46cad447e9421c708d
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-10 20:38:51 +0100

    test: throws when validation fails

--- apps/api/Tests/Validation/ValidationTests/ValidationBehaviorTests.cs
+++ apps/api/Tests/Validation/ValidationTests/ValidationBehaviorTests.cs
@@ -54,6 +54,35 @@ public class ValidationBehaviorTests
         Assert.True(nextCalled);
         Assert.Equal("ok", result);
     }
+
+    [Fact]
+    public async Task Handle_WhenValidatonFails_ThrowsRequestValidationException()
+    {
+        // Arrange
+        var validators = new IRequestValidator<TestRequest>[]
+        {
+            new InvalidTestRequestValidator()
+        };
+
+        var behavior = new ValidationBehavior<TestRequest, string>(validators);
+
+        var nextCalled = false;
+
+        Task<string> Next()
+        {
+            nextCalled = true;
+            return Task.FromResult("ok");
+        }
+
+        // Act
+        var exception = await Assert.ThrowsAsync<RequestValidationException>(
+            () => behavior.Handle(new TestRequest("", 0), CancellationToken.None, Next));
+
+        // Assert
+        Assert.False(nextCalled);
+        Assert.Single(exception.Errors);
+        Assert.Equal(nameof(TestRequest.Name), exception.Errors.First().Property);
+    }
         
     private sealed record TestRequest(string Name, int Quantity) : IRequest<string>;
 
@@ -62,6 +91,16 @@ public class ValidationBehaviorTests
         public IReadOnlyCollection<ValidationFailure> Validate(TestRequest request)
         {
             return [];
+        }
+    }
+
+    private class InvalidTestRequestValidator : IRequestValidator<TestRequest>
+    {
+        public IReadOnlyCollection<ValidationFailure> Validate(TestRequest request)
+        {
+            return [
+                new ValidationFailure(nameof(TestRequest.Name), "Name is required.")
+                ];
         }
     }
 }
