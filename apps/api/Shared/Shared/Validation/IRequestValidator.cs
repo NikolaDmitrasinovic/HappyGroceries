d8a42b826690d693ea60f5f28a7b4c555afc7108
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-03-10 14:55:27 +0100

    feat: add interface

---
+++ apps/api/Shared/Shared/Validation/IRequestValidator.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Validation;
+
+public interface IRequestValidator<in TRequest>
+{
+    IReadOnlyCollection<ValidationFailure> Validate(TRequest request);
+}
+
+public sealed record ValidationFailure(string Property, string Error);
