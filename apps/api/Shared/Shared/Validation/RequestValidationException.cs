f52c798154de04b62dd6ca8709f96f17f7d819b7
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-03-10 15:07:27 +0100

    feat: add exception

---
+++ apps/api/Shared/Shared/Validation/RequestValidationException.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Validation;
+
+public sealed class RequestValidationException(IReadOnlyCollection<ValidationFailure> errors) : Exception("One or more validation failures occurred.")
+{
+    public IReadOnlyCollection<ValidationFailure> Errors { get; } = errors;
+}
