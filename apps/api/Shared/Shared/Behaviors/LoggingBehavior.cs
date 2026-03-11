726c1924ad605bc3275ba6b982eac0e6cbfdc6af
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-03-11 13:28:55 +0100

    feat: add logging behavior

---
+++ apps/api/Shared/Shared/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using Shared.Messaging;
+using System.Diagnostics;
+
+namespace Shared.Behaviors;
+
+public sealed class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger = logger;
+
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        var requestName = typeof(TRequest).Name;
+        var stopwatch = Stopwatch.StartNew();
+
+        _logger.LogInformation("Handling request {RequestName}", requestName);
+
+        try
+        {
+            var response = await next();
+
+            stopwatch.Stop();
+
+            _logger.LogInformation("Handled request {RequestName} in {ElapsedMilliseconds} ms", requestName, stopwatch.ElapsedMilliseconds);
+
+            return response;
+        }
+        catch (Exception ex)
+        {
+            stopwatch.Stop();
+
+            _logger.LogError("Request {RequestName} failed after {ElapseMilliseconds} ms", requestName, stopwatch.ElapsedMilliseconds);
+
+            throw;
+        }
+    }
+}
