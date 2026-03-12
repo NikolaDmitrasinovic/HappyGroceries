e70c0aba6027c4b0a31a60f7cfe0971251e59fcb
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-12 19:27:27 +0100

    feat: add problem details factory

--- apps/api/Bootstrapper/Api/Errors/ProblemDetailsFactory.cs
+++ apps/api/Bootstrapper/Api/Errors/ProblemDetailsFactory.cs
@@ -1,5 +1,34 @@
-﻿namespace Api.Errors;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Errors;
 
 public class ProblemDetailsFactory
 {
+    public static ProblemDetails Create(HttpContext context, int statusCode, string title, string detail)
+    {
+        var problemDetails = new ProblemDetails
+        {
+            Title = title,
+            Detail = detail,
+            Status = statusCode,
+            Instance = context.Request.Path
+        };
+
+        problemDetails.Extensions["traceId"] = context.TraceIdentifier;
+
+        return problemDetails;
+    }
+
+    public static ProblemDetails CreateValidation(HttpContext context, IReadOnlyCollection<object> errors)
+    {
+        var problemDetails = Create(
+            context,
+            StatusCodes.Status400BadRequest,
+            "Validation failed.",
+            "One or more validation errors occurred.");
+
+        problemDetails.Extensions["errors"] = errors;
+
+        return problemDetails;
+    }
 }
