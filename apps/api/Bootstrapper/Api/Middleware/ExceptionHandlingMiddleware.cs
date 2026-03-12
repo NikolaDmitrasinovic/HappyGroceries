eda5dac97c91b51dcfcdf0183b9235a01386cc53
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-03-12 11:32:14 +0100

    feat: add exception handling middleware

---
+++ apps/api/Bootstrapper/Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Shared.Validation;
+
+namespace Api.Middleware;
+
+public class ExceptionHandlingMiddleware(
+    RequestDelegate next,
+    ILogger<ExceptionHandlingMiddleware> logger)
+{
+    private readonly RequestDelegate _next = next;
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger = logger;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (RequestValidationException ex)
+        {
+            await HandleValidationExceptionAsync(context, ex);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unhandled exception occurred.");
+
+            await HandleInternalServerErrorAsync(context);
+        }
+    }
+
+    private async Task HandleValidationExceptionAsync(HttpContext context, RequestValidationException ex)
+    {
+        throw new NotImplementedException();
+    }
+
+    private async Task HandleInternalServerErrorAsync(HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
