9f460976a2bc85db81fcde70aa36b8d8c35b2f0a
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-12 19:48:32 +0100

    feat: add exception to problem details mapper

---
+++ apps/api/Bootstrapper/Api/Errors/ExceptionToProblemDetailsMapper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Validation;
+
+namespace Api.Errors;
+
+public class ExceptionToProblemDetailsMapper
+{
+    public static ProblemDetails Map(HttpContext context, Exception exception)
+    {
+        return exception switch
+        {
+            RequestValidationException validationException
+                => ProblemDetailsFactory.CreateValidation(context, validationException.Errors),
+
+            _ => ProblemDetailsFactory.Create(
+                context,
+                StatusCodes.Status500InternalServerError,
+                "Server error",
+                "An unexpected error occured.")
+        };
+    }
+}
