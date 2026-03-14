f845a4ee2080772ac465a374c024ab9768cbd7f1
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-14 12:51:10 +0100

    feat: add command validatior to create product

--- apps/api/Modules/Inventory/Inventory/Products/Features/CreateProduct/CreateProductHandler.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/CreateProduct/CreateProductHandler.cs
@@ -1,9 +1,30 @@
-﻿namespace Inventory.Products.Features.CreateProduct;
+﻿using Shared.Validation;
+
+namespace Inventory.Products.Features.CreateProduct;
 
 public record CreateProductCommand(ProductDto Product)
     : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
+
+public sealed class CreateProductCommandValidatior : IRequestValidator<CreateProductCommand>
+{
+    public IReadOnlyCollection<ValidationFailure> Validate(CreateProductCommand request)
+    {
+        List<ValidationFailure> failures = [];
+
+        if (String.IsNullOrWhiteSpace(request.Product.Name))
+            failures.Add(new ValidationFailure(nameof(request.Product.Name), "Name is required."));
+
+        if (request.Product.Stock < 0)
+            failures.Add(new ValidationFailure(nameof(request.Product.Stock), "Stock cannot be negative."));
+
+        if (request.Product.Threshold < 0)
+            failures.Add(new ValidationFailure(nameof(request.Product.Threshold), "Threshold cannot be negative."));
+
+        return failures;
+    }
+}
 
 internal class CreateProductHandler(InventoryDbContext dbContext)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
