f9afafd26851ffb069ba1717e96aebd380fb900d
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-15 21:11:54 +0100

    feat: extract global using

--- apps/api/Modules/Inventory/Inventory/GlobalUsings.cs
+++ apps/api/Modules/Inventory/Inventory/GlobalUsings.cs
@@ -8,8 +8,9 @@ global using Shared.Data;
 global using Shared.Data.Seed;
 global using Shared.Validation;
 
-global using Inventory.Products.Models;
 global using Inventory.Data;
 global using Inventory.Data.Seed;
 global using Inventory.Products.Dtos;
+global using Inventory.Products.Exceptions;
+global using Inventory.Products.Models;
 global using Inventory.Products.Queries;
