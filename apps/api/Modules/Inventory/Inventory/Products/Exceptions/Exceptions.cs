7d68032bf9b10a4fb8b682446f3aeded02854e5c
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-15 21:11:34 +0100

    feat: add new exception

---
+++ apps/api/Modules/Inventory/Inventory/Products/Exceptions/Exceptions.cs
@@ -0,0 +1,5 @@
+﻿using Shared.Exceptions;
+
+namespace Inventory.Products.Exceptions;
+
+public class ProductNotFoundException(Guid Id) : NotFoundException($"Product with id '{Id}' was not found.");
