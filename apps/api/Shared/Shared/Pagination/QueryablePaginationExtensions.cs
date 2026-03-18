80f3cf97472cd4a7576cfdf10d4d7daa678952df
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-18 21:27:57 +0100

    feat: add to paginated result method

---
+++ apps/api/Shared/Shared/Pagination/QueryablePaginationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shared.Pagination;
+
+public static class QueryablePaginationExtensions
+{
+    public static async Task<PaginatedResult<T>> ToPaginatedResultAsync<T>(
+        this IQueryable<T> query,
+        PaginationRequest request,
+        CancellationToken cancellationToken = default)
+        where T : class
+    {
+        var pageIndex = request.PageIndex;
+        var pageSize = request.PageSize;
+
+        var totalCount = await query.LongCountAsync(cancellationToken);
+
+        var items = await query
+            .Skip(pageIndex * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        return new PaginatedResult<T>(
+            pageIndex,
+            pageSize,
+            totalCount,
+            items);
+    }
+}
