238ce19d8838f7c4753119f778e4dd6467951996
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-18 21:29:10 +0100

    refactor: improve code structure, get low stock

--- apps/api/Modules/Inventory/Inventory/Products/Features/GetLowStockProducts/GetLowStockProductsHandler.cs
+++ apps/api/Modules/Inventory/Inventory/Products/Features/GetLowStockProducts/GetLowStockProductsHandler.cs
@@ -9,27 +9,13 @@ internal class GetLowStockProductsHandler(InventoryDbContext dbContext)
 {
     public async Task<GetLowStockProductsResult> Handle(GetLowStockProductsQuery query, CancellationToken cancellationToken)
     {
-        var pageIndex = query.PaginationRequest.PageIndex;
-        var pageSize = query.PaginationRequest.PageSize;
-
-        var totalCount = await dbContext.Products.LongCountAsync(cancellationToken);
-
-        var products = await dbContext.Products
+        var pagedProducts = await dbContext.Products
             .AsNoTracking()
             .Where(ProductPredicates.IsLowStockExpression)
             .OrderBy(p => p.Name)
-            .ToListAsync(cancellationToken);
-
-        var productDtos = products
             .Select(p => new ProductDto(p.Name, p.Stock, p.Threshold))
-            .ToList();
+            .ToPaginatedResultAsync(query.PaginationRequest, cancellationToken);
 
-        return new GetLowStockProductsResult(
-            new PaginatedResult<ProductDto>(
-                pageIndex,
-                pageSize,
-                totalCount,
-                productDtos)
-            );
+        return new GetLowStockProductsResult(pagedProducts);
     }
 }
