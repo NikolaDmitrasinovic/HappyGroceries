ada903be46674e4343300f0d71b1354a1b147212
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-03-26 15:34:26 +0100

    feat: use AddMvc() to get supperted version in response headers

--- apps/api/Bootstrapper/Api/Program.cs
+++ apps/api/Bootstrapper/Api/Program.cs
@@ -22,7 +22,8 @@ builder.Services
         options.DefaultApiVersion = new ApiVersion(1, 0);
         options.AssumeDefaultVersionWhenUnspecified = true;
         options.ReportApiVersions = true;
-    });
+    })
+    .AddMvc();
 
 var app = builder.Build();
 
