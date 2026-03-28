6a479348c77fbd474c0989ad012d85d3f9228ab6
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-28 17:05:11 +0100

    feat: add props to Receipt

--- apps/api/Modules/Receipt/Receipt/Domain/Models/Receipt.cs
+++ apps/api/Modules/Receipt/Receipt/Domain/Models/Receipt.cs
@@ -4,5 +4,8 @@ namespace Receipt.Domain.Models;
 
 public class Receipt : Aggregate<Guid>
 {
+    public DateTime Date { get; set; }
+    public string Location { get; set; } = string.Empty;
+
     private Receipt() { }
 }
