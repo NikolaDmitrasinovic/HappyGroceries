441360903a8b434b23f357b73c15d4fedd17e3cf
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-28 20:42:51 +0100

    feat: add status as an enum

--- apps/api/Modules/Receipt/Receipt/Domain/Models/Receipt.cs
+++ apps/api/Modules/Receipt/Receipt/Domain/Models/Receipt.cs
@@ -7,10 +7,16 @@ public class Receipt : Aggregate<Guid>
     private readonly List<ReceiptLine> _lines = [];
 
     public DateTime PurchaseDate { get; private set; }
-    public bool Status { get; private set; }
+    public ReceiptStatus Status { get; private set; }
     public IReadOnlyList<ReceiptLine> Lines => _lines.AsReadOnly();
     public decimal TotalAmount => Lines.Sum(l => l.LineTotal);
     public string Location { get; private set; } = string.Empty;
 
     private Receipt() { }
+}
+
+public enum ReceiptStatus
+{
+    Open = 0,
+    Finalizes = 1
 }
