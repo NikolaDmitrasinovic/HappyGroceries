772c8fe5273c1e1cc1968c0f8754e55181bc61d9
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-28 20:29:18 +0100

    feat: imprve props

--- apps/api/Modules/Receipt/Receipt/Domain/Models/Receipt.cs
+++ apps/api/Modules/Receipt/Receipt/Domain/Models/Receipt.cs
@@ -4,9 +4,12 @@ namespace Receipt.Domain.Models;
 
 public class Receipt : Aggregate<Guid>
 {
+    private readonly List<ReceiptLine> _lines = [];
+
     public DateTime PurchaseDate { get; set; }
     public bool Status { get; set; }
-    public decimal TotalAmount { get; set; }
+    public IReadOnlyList<ReceiptLine> Lines => _lines.AsReadOnly();
+    public decimal TotalAmount => Lines.Sum(l => l.LineTotal);
     public string Location { get; set; } = string.Empty;
 
     private Receipt() { }
