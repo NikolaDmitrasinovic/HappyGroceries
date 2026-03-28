4189827de1682ec257c4d3543bf7bc0e24016085
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-28 17:05:21 +0100

    feat: add props to receipt line

--- apps/api/Modules/Receipt/Receipt/Domain/Models/ReceiptLine.cs
+++ apps/api/Modules/Receipt/Receipt/Domain/Models/ReceiptLine.cs
@@ -4,4 +4,11 @@ namespace Receipt.Domain.Models;
 
 public class ReceiptLine : Entity<Guid>
 {
+    public string ProductName { get; set; } = default!;
+    public decimal UnitPrice { get; set; }
+    public int Quantity { get; set; }
+
+    public decimal LineTotal => UnitPrice * Quantity;
+
+    private ReceiptLine() { }
 }
