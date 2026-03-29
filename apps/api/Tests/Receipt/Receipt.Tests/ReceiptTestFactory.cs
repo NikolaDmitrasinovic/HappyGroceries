1280361b220c6e7093bb55d687c5244fd44e3691
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-29 17:59:12 +0200

    test: extract receipt factory

---
+++ apps/api/Tests/Receipt/Receipt.Tests/ReceiptTestFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Receipt.Tests;
+
+public static class ReceiptTestFactory
+{
+    public static Domain.Models.Receipt CreateReceipt(
+        string purchaseDate = "2026-04-07",
+        string location = "some-location")
+    {
+        var date = DateOnly.Parse(purchaseDate);
+
+        var receipt = Domain.Models.Receipt.Open(date, location);
+        receipt.ClearDomainEvents();
+        return receipt;
+    }
+}
