a0a8b743060354e843067df1e6c18d68c72737ec
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-30 17:55:50 +0200

    feat: add receipt configuration

---
+++ apps/api/Modules/Receipt/Receipt/Infrastructure/Configurations/PurchaseReceiptConfiguration.cs
@@ -0,0 +1,30 @@
+﻿namespace Receipt.Infrastructure.Configurations;
+
+public class PurchaseReceiptConfiguration : IEntityTypeConfiguration<PurchaseReceipt>
+{
+    public void Configure(EntityTypeBuilder<PurchaseReceipt> builder)
+    {
+        builder.ToTable("purchase_receipts");
+
+        builder.HasKey(pr => pr.Id);
+
+        builder.Property(pr => pr.PurchaseDate)
+            .IsRequired();
+
+        builder.Property(pr => pr.Status)
+            .IsRequired();
+
+        builder.Property(pr => pr.TotalAmount)
+            .HasPrecision(18, 2)
+            .IsRequired();
+
+        builder.Property(pr => pr.Location)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.HasMany(pr => pr.Lines)
+            .WithOne()
+            .HasForeignKey(rl => rl.ReceiptId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
