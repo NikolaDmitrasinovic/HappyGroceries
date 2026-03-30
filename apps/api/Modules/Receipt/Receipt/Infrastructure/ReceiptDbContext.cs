dfbbb8ce78396f3036c88816ece494bda6a07ecc
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-03-30 17:56:10 +0200

    feat: implement db context

--- apps/api/Modules/Receipt/Receipt/Infrastructure/ReceiptDbContext.cs
+++ apps/api/Modules/Receipt/Receipt/Infrastructure/ReceiptDbContext.cs
@@ -1,9 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Receipt.Infrastructure;
 
-namespace Receipt.Infrastructure;
-
-internal class ReceiptDbContext
+public class ReceiptDbContext(DbContextOptions<ReceiptDbContext> options) : DbContext(options)
 {
+    public DbSet<PurchaseReceipt> PurchaseReceipts => Set<PurchaseReceipt>();
+    public DbSet<ReceiptLine> ReceiptLines => Set<ReceiptLine>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("receipt");
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
