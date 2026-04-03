06c434669cc5d4442509d39f293153478ba29b95
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-04-03 16:13:33 +0200

    feat: add command

---
+++ apps/api/Modules/Receipt/Receipt/Application/Features/OpenPurchaseReceipt/OpenPurchaseReceiptCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace Receipt.Application.Features.OpenPurchaseReceipt;
+
+public record OpenPurchaseReceiptCommand(PurchaseReceiptDto PurchaseReceipt)
+    : ICommand<OpenPurchaseReceiptResult>;
+
+public record OpenPurchaseReceiptResult(Guid Id);
