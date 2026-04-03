9cc6c7a2040757b1a92d4511e220676b8c261581
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-04-03 16:13:45 +0200

    feat: add handler

---
+++ apps/api/Modules/Receipt/Receipt/Application/Features/OpenPurchaseReceipt/OpenPurchaseReceiptHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Receipt.Application.Features.OpenPurchaseReceipt;
+
+internal class OpenPurchaseReceiptHandler(ReceiptDbContext dbContext) : ICommandHandler<OpenPurchaseReceiptCommand, OpenPurchaseReceiptResult>
+{
+    public async Task<OpenPurchaseReceiptResult> Handle(OpenPurchaseReceiptCommand request, CancellationToken cancellationToken)
+    {
+        PurchaseReceipt purchaseReceipt = CreateOpenReceipt(request.PurchaseReceipt);
+
+        await dbContext.PurchaseReceipts.AddAsync(purchaseReceipt, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new OpenPurchaseReceiptResult(purchaseReceipt.Id);
+    }
+
+    private static PurchaseReceipt CreateOpenReceipt(PurchaseReceiptDto request)
+    {
+        return PurchaseReceipt.Open(
+            request.PurchaseDate,
+            request.Location);
+    }
+}
