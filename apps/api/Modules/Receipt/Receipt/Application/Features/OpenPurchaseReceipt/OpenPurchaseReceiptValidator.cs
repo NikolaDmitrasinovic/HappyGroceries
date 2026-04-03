9cfa2b8ecc04567f47622cb26918bad70a1bb730
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-04-03 16:13:53 +0200

    feat: add validation

---
+++ apps/api/Modules/Receipt/Receipt/Application/Features/OpenPurchaseReceipt/OpenPurchaseReceiptValidator.cs
@@ -0,0 +1,17 @@
+﻿using Shared.Abstractions.Time;
+
+namespace Receipt.Application.Features.OpenPurchaseReceipt;
+
+internal class OpenPurchaseReceiptValidator(IClock clock) : IRequestValidator<OpenPurchaseReceiptCommand>
+{
+    private readonly IClock _clock = clock;
+    public IReadOnlyCollection<ValidationFailure> Validate(OpenPurchaseReceiptCommand request)
+    {
+        List<ValidationFailure> failures = [];
+
+        if (request.PurchaseReceipt.PurchaseDate > DateOnly.FromDateTime(_clock.UtcNow))
+            failures.Add(new ValidationFailure(nameof(request.PurchaseReceipt.PurchaseDate), "Purchase date cannot be in the future."));
+
+        return failures;
+    }
+}
