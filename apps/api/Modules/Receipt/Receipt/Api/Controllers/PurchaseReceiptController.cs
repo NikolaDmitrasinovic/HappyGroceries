6c2d40c8c095492c1d18e286840efec8c49fa25d
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-04-03 20:09:06 +0200

    feat: add from body to create endpoint

--- apps/api/Modules/Receipt/Receipt/Api/Controllers/PurchaseReceiptController.cs
+++ apps/api/Modules/Receipt/Receipt/Api/Controllers/PurchaseReceiptController.cs
@@ -9,13 +9,13 @@ namespace Receipt.Api.Controllers;
 
 [ApiController]
 [ApiVersion("1.0")]
-[Route("api/v{version:apiVersion}/receipt/purchase")]
+[Route("api/v{version:apiVersion}/receipt/purchase-receipts")]
 public class PurchaseReceiptController(IMediator mediator) : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(OpenPurchaseReceiptResponse), StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<ActionResult<OpenPurchaseReceiptResponse>> Create(OpenPurchaseReceiptRequest request, CancellationToken cancellationToken)
+    public async Task<ActionResult<OpenPurchaseReceiptResponse>> Create([FromBody] OpenPurchaseReceiptRequest request, CancellationToken cancellationToken)
     {
         var result = await mediator.Send(new OpenPurchaseReceiptCommand(request.PurchaseDate, request.Location), cancellationToken);
         var response = new OpenPurchaseReceiptResponse(result.Id);
