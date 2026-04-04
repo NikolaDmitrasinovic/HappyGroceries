cdce457ad4272c7f64e0551ed0a101cc387895ce
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-04-04 21:46:52 +0200

    feat: add command

---
+++ apps/api/Modules/Receipt/Receipt/Application/Features/AddLineToReceipt/AddLineToReceiptCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace Receipt.Application.Features.AddLineToReceipt;
+
+public record AddLineToReceiptCommand(Guid ReceiptId, string ProductName, decimal UnitPrice, int Quantity) : IRequest<AddLineToReceiptResult>;
+
+public record AddLineToReceiptResult(Guid ReceiptLineId);
