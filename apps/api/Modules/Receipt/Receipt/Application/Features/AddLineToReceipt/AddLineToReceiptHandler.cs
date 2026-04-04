6a380ce7002ccf2cccffda349041146977ee87b8
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-04-04 22:17:23 +0200

    feat: add handler

---
+++ apps/api/Modules/Receipt/Receipt/Application/Features/AddLineToReceipt/AddLineToReceiptHandler.cs
@@ -0,0 +1,18 @@
+﻿using Shared.Exceptions;
+
+namespace Receipt.Application.Features.AddLineToReceipt;
+
+internal class AddLineToReceiptHandler(ReceiptDbContext dbContext) : IRequestHandler<AddLineToReceiptCommand, AddLineToReceiptResult>
+{
+    public async Task<AddLineToReceiptResult> Handle(AddLineToReceiptCommand request, CancellationToken cancellationToken)
+    {
+        var _ = await dbContext.PurchaseReceipts.FindAsync([request.ReceiptId], cancellationToken) ?? throw new NotFoundException($"Receipt with id {request.ReceiptId} not found.");
+
+        var receiptLine = ReceiptLine.Create(request.ReceiptId, request.ProductName, request.UnitPrice, request.Quantity);
+
+        await dbContext.ReceiptLines.AddAsync(receiptLine, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new AddLineToReceiptResult(receiptLine.Id);
+    }
+}
