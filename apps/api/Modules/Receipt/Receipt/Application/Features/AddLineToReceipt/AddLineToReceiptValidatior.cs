0f45e02dad02044a9c5ccc80c1c2ec533f10d8dd
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-04-04 22:17:32 +0200

    feat: add validator

---
+++ apps/api/Modules/Receipt/Receipt/Application/Features/AddLineToReceipt/AddLineToReceiptValidatior.cs
@@ -0,0 +1,20 @@
+﻿namespace Receipt.Application.Features.AddLineToReceipt;
+
+public class AddLineToReceiptValidatior : IRequestValidator<AddLineToReceiptCommand>
+{
+    public IReadOnlyCollection<ValidationFailure> Validate(AddLineToReceiptCommand request)
+    {
+        var failures = new List<ValidationFailure>();
+
+        if (string.IsNullOrEmpty(request.ProductName))
+            failures.Add(new ValidationFailure(nameof(request.ProductName), "Product name is required."));
+
+        if (request.UnitPrice < 0)
+            failures.Add(new ValidationFailure(nameof(request.UnitPrice), "Price cannot be less than 0."));
+
+        if (request.Quantity <= 0)
+            failures.Add(new ValidationFailure(nameof(request.Quantity), "Quantity must be greater than 0."));
+
+        return failures;
+    }
+}
