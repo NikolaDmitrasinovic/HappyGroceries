4e8cdb0abe0e7e20fe3bcc98cf966ae96d1f36fe
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-04-05 11:18:55 +0200

    chore: rename param

--- apps/api/Modules/Receipt/Receipt/Api/Contracts/AddLineToReceiptContract.cs
+++ apps/api/Modules/Receipt/Receipt/Api/Contracts/AddLineToReceiptContract.cs
@@ -1,5 +1,5 @@
 ﻿namespace Receipt.Api.Contracts;
 
-public record AddLineToReceiptRequest(Guid ReceiptId, string ProductName, decimal Unitprice, int Quantity);
+public record AddLineToReceiptRequest(Guid ReceiptId, string ProductName, decimal UnitPrice, int Quantity);
 
 public record AddLineToReceiptResponse(Guid ReceiptLineId);
