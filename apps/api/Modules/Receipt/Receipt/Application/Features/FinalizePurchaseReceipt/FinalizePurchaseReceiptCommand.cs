52885e3b4dd195f09c4aab096ef339e5477604b3
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2026-04-05 15:39:35 +0200

    feat: add command

---
+++ apps/api/Modules/Receipt/Receipt/Application/Features/FinalizePurchaseReceipt/FinalizePurchaseReceiptCommand.cs
@@ -0,0 +1,5 @@
+﻿namespace Receipt.Application.Features.FinalizePurchaseReceipt;
+
+public record FinalizePurchaseReceiptCommand(Guid ReceiptId) : ICommand<FinalizePurchaseReceiptResult>;
+
+public record FinalizePurchaseReceiptResult(Guid ReceiptId);
