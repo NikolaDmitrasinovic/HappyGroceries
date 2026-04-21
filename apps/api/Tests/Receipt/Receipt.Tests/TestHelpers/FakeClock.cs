924d4b1488e1497f46654ce96389125238986706
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-04-21 13:20:14 +0200

    test: add fake clock helper

---
+++ apps/api/Tests/Receipt/Receipt.Tests/TestHelpers/FakeClock.cs
@@ -0,0 +1,8 @@
+﻿using Shared.Abstractions.Time;
+
+namespace Receipt.Tests.TestHelpers;
+
+public class FakeClock(DateTime utcNow) : IClock
+{
+    public DateTime UtcNow { get; set; } = utcNow;
+}
