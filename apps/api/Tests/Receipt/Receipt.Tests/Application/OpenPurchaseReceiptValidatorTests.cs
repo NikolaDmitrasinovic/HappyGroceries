bfa86c5fa10b38433ad8fc9b8c77a808fe3468b4
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-04-21 13:20:43 +0200

    test: validate returns no failures

---
+++ apps/api/Tests/Receipt/Receipt.Tests/Application/OpenPurchaseReceiptValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using Receipt.Application.Features.OpenPurchaseReceipt;
+using Receipt.Tests.TestHelpers;
+
+namespace Receipt.Tests.Application;
+
+public class OpenPurchaseReceiptValidatorTests
+{
+    [Fact]
+    public void Validate_Returns_No_Failures_When_PurchaseDate_Is_Valid()
+    {
+        // Arrange
+        var clock = new FakeClock(new DateTime(2026, 4, 24, 10, 0 , 0, DateTimeKind.Utc));
+        var validator = new OpenPurchaseReceiptValidator(clock);
+
+        var request = new OpenPurchaseReceiptCommand(
+            new DateOnly(2026, 4, 20),
+            "some-location");
+
+        // Act
+        var failures = validator.Validate(request);
+
+        // Assert
+        Assert.Empty(failures);
+    }
+}
