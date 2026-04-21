216244051c9cedcb499d94c683e87bd8e454defe
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-04-21 12:23:21 +0200

    feat: make date nullable

--- apps/api/Modules/Receipt/Receipt/Api/Contracts/OpenPurchaseReceiptContract.cs
+++ apps/api/Modules/Receipt/Receipt/Api/Contracts/OpenPurchaseReceiptContract.cs
@@ -1,5 +1,5 @@
 ﻿namespace Receipt.Api.Contracts;
 
-public record OpenPurchaseReceiptRequest(DateOnly PurchaseDate, string? Location);
+public record OpenPurchaseReceiptRequest(DateOnly? PurchaseDate, string? Location);
 
 public record OpenPurchaseReceiptResponse(Guid Id);
