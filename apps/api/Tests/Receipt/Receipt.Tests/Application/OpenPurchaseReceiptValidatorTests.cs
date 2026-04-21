e6a744251761c86fef668d986b295892aa5f1d00
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-04-21 13:31:46 +0200

    test: add test

--- apps/api/Tests/Receipt/Receipt.Tests/Application/OpenPurchaseReceiptValidatorTests.cs
+++ apps/api/Tests/Receipt/Receipt.Tests/Application/OpenPurchaseReceiptValidatorTests.cs
@@ -22,4 +22,64 @@ public class OpenPurchaseReceiptValidatorTests
         // Assert
         Assert.Empty(failures);
     }
+
+    [Fact]
+    public void Validate_Returns_Failure_When_PurchaseDate_Is_In_The_Future()
+    {
+        // Arrange
+        var clock = new FakeClock(new DateTime(2026, 4, 24, 10, 0, 0, DateTimeKind.Utc));
+        var validator = new OpenPurchaseReceiptValidator(clock);
+
+        var request = new OpenPurchaseReceiptCommand(
+            new DateOnly(2026, 8, 30),
+            "some-location");
+
+        // Act
+        var failures = validator.Validate(request);
+
+        // Assert
+        Assert.Single(failures);
+        Assert.Equal(nameof(OpenPurchaseReceiptCommand.PurchaseDate), failures.First().Property);
+    }
+
+    [Fact]
+    public void Validate_ReturnsFailure_When_PurchaseDate_Is_Too_Old()
+    {
+        // Right now too old is hard coded as older than 10 years
+
+        // Arrange
+        var clock = new FakeClock(new DateTime(2026, 4, 24, 10, 0, 0, DateTimeKind.Utc));
+        var validator = new OpenPurchaseReceiptValidator(clock);
+
+        var request = new OpenPurchaseReceiptCommand(
+            new DateOnly(2000, 4, 24),
+            "some-location");
+
+        // Act
+        var failures = validator.Validate(request);
+
+        // Assert
+        Assert.Single(failures);
+        Assert.Equal(nameof(OpenPurchaseReceiptCommand.PurchaseDate), failures.First().Property);
+    }
+
+    [Fact]
+    public void Validate_Returns_No_Failures_When_PurchaseDate_Equals_MinimumDate()
+    {
+        // Right now minimumDate is hard coded as 10 years ago
+
+        // Arrange
+        var clock = new FakeClock(new DateTime(2026, 4, 24, 10, 0, 0, DateTimeKind.Utc));
+        var validator = new OpenPurchaseReceiptValidator(clock);
+
+        var request = new OpenPurchaseReceiptCommand(
+            new DateOnly(2016, 4, 24),
+            "some-location");
+
+        // Act
+        var failures = validator.Validate(request);
+
+        // Assert
+        Assert.Empty(failures);
+    }
 }
