5d3a0d96396ffc289b0439c0c63fbb2c062f01f2
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-05-14 15:05:36 +0200

    feat: make product id nullable

--- apps/api/Modules/Receipt/Receipt/Domain/Models/ReceiptLine.cs
+++ apps/api/Modules/Receipt/Receipt/Domain/Models/ReceiptLine.cs
@@ -3,7 +3,7 @@
 public class ReceiptLine : Entity<Guid>
 {
     public Guid ReceiptId { get; private set; }
-    public Guid ProductId { get; private set; }
+    public Guid? ProductId { get; private set; }
     public string ProductName { get; private set; } = default!;
     public decimal UnitPrice { get; private set; }
     public int Quantity { get; private set; }
@@ -12,7 +12,7 @@ public class ReceiptLine : Entity<Guid>
 
     private ReceiptLine() { }
 
-    public static ReceiptLine Create(Guid receiptId, Guid productId, string productName, decimal unitPrice, int quantity)
+    public static ReceiptLine Create(Guid receiptId, Guid? productId, string productName, decimal unitPrice, int quantity)
     {
         if (receiptId == Guid.Empty)
             throw new ArgumentException("Receipt id cannot be empty.", nameof(receiptId));
