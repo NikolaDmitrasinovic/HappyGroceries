45a29c56d9996e891c8017149dc8b63ff3bba133
Nikola Dmitrasinovic <nikola.dmitrasinovic@brighting.nl>
2026-05-22 14:52:45 +0200

    feat: add consume stock handler

---
+++ apps/api/Modules/Inventory/Inventory/Products/Features/ConsumeProductStock/ConsumeProductStockHandler.cs
@@ -0,0 +1,40 @@
+namespace Inventory.Products.Features.ConsumeProductStock;
+
+public record ConsumeProductStockCommand(Guid Id, int Delta)
+    : ICommand<ConsumeProductStockResult>;
+    
+public record ConsumeProductStockResult(Guid Id);
+
+internal sealed class ConsumeProductStockValidator : IRequestValidator<ConsumeProductStockCommand>
+{
+    public IReadOnlyCollection<ValidationFailure> Validate(ConsumeProductStockCommand request)
+    {
+        List<ValidationFailure> failures = [];
+
+        if (request.Id == Guid.Empty)
+            failures.Add(new ValidationFailure(nameof(request.Id), "Id is required."));
+
+        if (request.Delta == 0)
+            failures.Add(new ValidationFailure(nameof(request.Delta), "Delta cannot be 0."));
+        
+        if (request.Delta < 0)
+            failures.Add(new ValidationFailure(nameof(request.Delta), "Delta cannot be negative."));
+
+        return failures;
+    }
+}
+
+internal class ConsumeProductStockHandler(InventoryDbContext dbContext)
+    : ICommandHandler<ConsumeProductStockCommand, ConsumeProductStockResult>
+{
+    public async Task<ConsumeProductStockResult> Handle(ConsumeProductStockCommand command, CancellationToken cancellationToken)
+    {
+        var product = await dbContext.Products.FindAsync([command.Id], cancellationToken)
+                      ?? throw new ProductNotFoundException(command.Id);
+
+        product.ConsumeStock(command.Delta);
+        await dbContext.SaveChangesAsync(cancellationToken);
+        
+        return new ConsumeProductStockResult(product.Id);
+    }
+}
